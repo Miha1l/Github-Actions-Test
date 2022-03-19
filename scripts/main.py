c77f366434df3011d370f99d123dc0c8e0f44808
Miha1l <mr.loykonen@mail.ru>
2022-03-20 02:10:41 +0300

    upd files

--- scripts/main.py
+++ scripts/main.py
@@ -5,8 +5,10 @@ import time
 def main():
     data = sys.argv[1]
     date = time.strptime(data, '%Y-%m-%dT%H:%M:%SZ')
-    print(f'Дата создания: {date.tm_year}-{date.tm_mon}-{date.tm_mday}')
-    print(f'Время создания: {date.tm_hour}-{date.tm_min}-{date.tm_sec}')
+    creation_date = time.strftime('%Y-%m-%d', date)
+    creation_time = time.strftime('%H:%M:%S', date)
+    print(f'Дата создания: {creation_date}')
+    print(f'Время создания: {creation_time}')
     return
 
 
